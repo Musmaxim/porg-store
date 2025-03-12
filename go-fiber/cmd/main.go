5732ec42fe0cdd43c6956539ece6723b9c12bcd2
unknown <musmaxim@gmail.com>
2025-03-12 21:09:34 +0300

    added log

--- go-fiber/cmd/main.go
+++ go-fiber/cmd/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"go-fiber/config"
 	"go-fiber/internal/home"
-
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 )
 
 func main() {
+	config.Init()
+	config.NewDatabaseConfig()
+	logConfig := config.NewLogConfig()
 	app := fiber.New()
+	log.SetLevel(log.Level(logConfig.Level))
+	app.Use(logger.New())
+	app.Use(recover.New())
 
 	home.NewHomeHandler(app)
 
--- go-fiber/internal/home/handler.go
+++ go-fiber/internal/home/handler.go
@@ -1,17 +1,26 @@
 package home
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/log"
+)
 
 type HomeHandler struct {
 	router fiber.Router
 }
 
-func NewHomeHandler(root fiber.Router) *HomeHandler {
+func NewHomeHandler(root fiber.Router) {
 	h := &HomeHandler{router: root}
-	h.router.Get("/", h.home)
-	return h
+	api := h.router.Group("/api")
+	api.Get("/", h.home)
+	api.Get("/error", h.error)
 }
 
 func (h *HomeHandler) home(c *fiber.Ctx) error {
 	return c.SendString("Hello World!")
+}
+
+func (h *HomeHandler) error(c *fiber.Ctx) error {
+	log.Info("Error!")
+	return c.SendString("Error!")
 }
